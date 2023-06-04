75c35d07791ca1275c6bd53e7f75f31fcb322e07
Dani <fornasari06@gmail.com>
2023-06-04 15:56:48 +0200

    2023/06/04

--- public/nodejs/server/src/index.js
+++ public/nodejs/server/src/index.js
@@ -1,14 +1,15 @@
+const port = 3000
 import express from 'express'
 const app = express()
-const port = 3000
-
 import bodyParser from 'body-parser'
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-    res.send('server_0_default')
+    res.send('server_?')
 })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+// import fs from 'node:fs/promises'
